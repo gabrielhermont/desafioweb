4b16c7f9cc88cae4f559786e2c2b3c6684117622
Gabriel <gabrielhermont@cpejr.com.br>
2023-11-01 16:48:05 -0300

    Começo Desafio Web
    Tentando arrumar o início da página

---
+++ Desafio_web/src/App.jsx
@@ -0,0 +1,30 @@
+import "./app.css"
+
+function App() {
+
+  return (
+    <div>
+      <body>
+        <div className="centro_login">
+
+        Usuário
+        <input></input>
+        Senha
+        <input></input>
+        Entrar
+        Cadastro
+
+        </div>
+        <div className="nuvens">
+
+
+        </div>
+      </body>
+    
+    </div>
+ 
+  )
+}
+
+export default App
+
